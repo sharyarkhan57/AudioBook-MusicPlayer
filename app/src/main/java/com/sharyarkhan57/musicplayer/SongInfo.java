e95d22aa36955ab88933df5f61d4a89b790a4c55
Sharyar Khan <sharyarkhan57@gmail.com>
2017-10-23 11:58:04 -0400

    This is the current audio player app with basic functionality. Users can load music from sd card and play, as well as stream from online sources. *TODO* -clean up interface -add fine scrolling/chapter jumping -create organizational hierarchy for albums

---
+++ app/src/main/java/com/sharyarkhan57/musicplayer/SongInfo.java
@@ -0,0 +1,44 @@
+package com.sharyarkhan57.musicplayer;
+
+/**
+ * Created by Sharyar Khan
+ */
+
+public class SongInfo {
+    private String Songname;
+    private String Artistname;
+    private String SongUrl;
+
+    public SongInfo() {
+    }
+
+    public SongInfo(String songname, String artistname, String songUrl) {
+        Songname = songname;
+        Artistname = artistname;
+        SongUrl = songUrl;
+    }
+
+    public String getSongname() {
+        return Songname;
+    }
+
+    public void setSongname(String songname) {
+        Songname = songname;
+    }
+
+    public String getArtistname() {
+        return Artistname;
+    }
+
+    public void setArtistname(String artistname) {
+        Artistname = artistname;
+    }
+
+    public String getSongUrl() {
+        return SongUrl;
+    }
+
+    public void setSongUrl(String songUrl) {
+        SongUrl = songUrl;
+    }
+}
